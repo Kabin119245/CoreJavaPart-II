b9b1e37f3e712395cb2a120742f98d79dbd21f25
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-19 16:23:44 +0545

    updated

--- Sam.java
+++ Sam.java
@@ -3,12 +3,35 @@
 interface A {
     void show();
 }
-
+interface B {
+    void show1(int i);
+}
 //etiher we can implement above method using class or we can use anonymous inner class, which can also be modified using lambda expression
 public class Sam {
     public static void main(String[] args) {
 
         A obj = ()  -> System.out.println("in new show");
         obj.show();
+    
+
+
+    //what we used to do 
+        // B obj1 = new B(){
+        
+        //     public void show1(int i) {
+        //         System.out.println("In B show" + i);
+        //     }
+        // };
+        // obj1.show1(10);
+
+
+        //we can do it by lambda expression as
+
+        B obj1 = i -> System.out.println("In B show" + i);
+           
+        
+        obj1.show1(10);
+
+
     }
 }
