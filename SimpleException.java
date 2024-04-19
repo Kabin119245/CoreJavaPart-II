336af9b01a4d529cef79a57e823084d64de1bb83
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-19 18:23:14 +0545

    More exceptions added

--- SimpleException.java
+++ SimpleException.java
@@ -2,15 +2,28 @@
 public class SimpleException {
 
     public static void main(String[] args) {
-        int i = 0;
+        int i = 2;
         int j = 0;
+        int nums[] = new int[5];
+
+        String str=null;
 
         try {
             j = 18/i;
+            System.out.println(str.length());
+            System.out.println(nums[5]);
+            
         }
 
-        catch (Exception e) {
+        catch (ArithmeticException e) {
              System.out.println(e.getMessage());
+        }
+         catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        //Exception is (parent) class, make sure it is at bottom
+        catch(Exception e) {
+            System.out.println("Something went wrong");
         }
 
         System.out.println(j);
