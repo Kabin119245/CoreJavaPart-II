1df99864a52a7c1bcb91e257da3093115e274bda
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-19 22:07:10 +0545

    try-finally

---
+++ FinallyBlock.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FinallyBlock {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int num = 0;
+        BufferedReader br = null;
+
+        try {
+            InputStreamReader in = new InputStreamReader(System.in);
+            br = new BufferedReader(in);
+
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        } 
+
+      
+
+        finally {
+            //in finally block we close the resource, beacuse it is going to be executed no matter exception occurs or not
+            System.out.println("I am goin to execute irrespective of the exception");
+
+            br.close();
+        }
+    }
+}
