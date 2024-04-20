d15db70165ba97e87270413f2e75ec8197f75d79
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-20 22:06:44 +0545

    map added

---
+++ UsingMap.java
@@ -0,0 +1,36 @@
+//while using List or ArrayList we know that they have index value like 0,1,2,3 what if we want different kinds of index values like any name in that case we use map, it has a key and value
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsingMap {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> students = new HashMap<String, Integer>();
+        //HashTable also
+
+        students.put("Kabin", 100);
+        students.put("Kabina", 85);
+        students.put("Kanchan", 85);
+        students.put("Asmita", 75);
+        students.put("Anushree", 70);
+
+        System.out.println(students);
+        // to get individual we use key
+
+        System.out.println(students.get("Kabin"));
+
+        // to change
+        students.put("Anushree", 85);
+        // as we are not geting new Anushree, so we conclude that key cannot be repeated
+
+        // to print one by one we use enhanced for loop
+
+        for (String key : students.keySet()) {
+            System.out.println(key + " : " + students.get(key));
+        }
+
+    }
+
+}
