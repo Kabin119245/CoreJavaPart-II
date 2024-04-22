3618c7f97f42c2ab5b16e7281ffb7755ea12680e
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-22 13:58:48 +0545

    added

---
+++ SealedClass.java
@@ -0,0 +1,21 @@
+//if we want that only certain class can inherit class A then there is a Sealed Class
+sealed class A permits B,C {
+
+}
+//the permitted class should be sealed, non-sealed or final class
+non-sealed class B extends A { //sealed, non-sealed or final
+
+}
+
+final class C extends A {
+
+}
+
+class D {
+
+}
+
+
+public class SealedClass {
+    
+}
